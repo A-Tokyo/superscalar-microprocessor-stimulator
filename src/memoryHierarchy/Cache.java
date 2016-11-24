18e26294a5874a7b640c5dbfb9853a1ed05f50f2
Tokyo <ahmed.tokyo1@gmail.com>
2016-11-25 01:23:45 +0200

    full cache constructor

--- src/memoryHierarchy/Cache.java
+++ src/memoryHierarchy/Cache.java
@@ -4,12 +4,31 @@ public class Cache {
 	int size; // size of cache
 	int lineSize; // line size of cache 
 	int m; // associativity
+	int writePolicyHit; // 0 for writeThrough 1 for writeBack
+	int writePolicyMiss; // 0 for writeThrough 1 for writeBack
+	int accessCycles; // access time (in cycles)
 	
 	Set [] sets;
 	
 	int totalHits;
 	int totalMisses;
 	
+	public Cache(int size, int lineSize, int m, int writePolicyHit, int writePolicyMiss, int accessCycles) {
+		this.size = size;
+		this.lineSize = lineSize;
+		this.m = m;
+		this.writePolicyHit = writePolicyHit;
+		this.writePolicyMiss = writePolicyMiss;
+		this.accessCycles = accessCycles;
+		
+		// size over line size to get number of blocks in cache, /m since there are m blocks per set.
+		this.sets = new Set[size/lineSize/m];
+		for (int i=0; i < this.sets.length; i++) {
+			// each set is a new set of size m and lineSize lineSize
+			this.sets[i] = new Set(m, lineSize);
+		}
+	}
+
 	public int getSize() {
 		return size;
 	}
