824264ea02b4fef53fbd5c247fd073f165cc6002
Tokyo <ahmed.tokyo1@gmail.com>
2016-11-25 18:34:15 +0200

    add decimalToBinary

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -1,10 +1,27 @@
 package utils;
 
 public class Utils {
+	
+	private static final int wordSizeInBits = 16;
 
 	public static int log2(int num){
 		// Log base x of n is log n / log x
 		return (int)(Math.log(num)/Math.log(2));
+	}
+	
+	// Takes a number in decimal and returns the 16 bit binary representation in String form
+	public static String decimalToBinary(int number) {
+		String toReturn = "";
+		int remainder = 0;
+		while (number != 0) {
+			remainder = number % 2;
+			toReturn = remainder + toReturn;
+			number /= 2;
+		}
+		while(toReturn.length() < wordSizeInBits){
+			toReturn = 0 + toReturn;
+		}
+		return toReturn;
 	}
 	
 }
