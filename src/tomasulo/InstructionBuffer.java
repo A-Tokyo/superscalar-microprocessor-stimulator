c4c0875fcdb0f3fae0201c7f6a386811b08443dd
Mostafa Ebrahim <mostafa.ebrahim@hotmail.com>
2016-11-26 03:37:49 +0200

    instruction beffer
    lsa 3ayz ykml  wa2f feh bas l7d ma 23ml l instructuions

---
+++ src/tomasulo/InstructionBuffer.java
@@ -0,0 +1,19 @@
+package tomasulo;
+
+public class InstructionBuffer {
+
+	//  Instruction  : hna lazm bas 23ml l instruction hakmlha b3dan 
+	int head;
+	int tail;
+
+		
+	public InstructionBuffer() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
