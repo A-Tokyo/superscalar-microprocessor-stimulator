721035e928ece0fb55a5dfd261000ced83612760
Mostafa Ebrahim <mostafa.ebrahim@hotmail.com>
2016-11-26 21:29:18 +0200

    Creation of FU files tree
    mogrd   rasm 2aw ta5yl lel  mafrod ykon mawgod

---
+++ src/functionalUnits/Load_Store_FunctionalUnit.java
@@ -0,0 +1,15 @@
+package functionalUnits;
+
+
+public class Load_Store_FunctionalUnit extends FunctionalUnit {
+	
+	int cycles;
+	public Load_Store_FunctionalUnit(int cycles) {
+		// TODO Auto-generated constructor stub
+		this.cycles = cycles;
+	}
+
+
+
+
+}
---
+++ src/functionalUnits/MultiplyFunctionalUnit.java
@@ -0,0 +1,17 @@
+package functionalUnits;
+
+
+public class MultiplyFunctionalUnit extends FunctionalUnit {
+
+	int cycles;
+	
+	public MultiplyFunctionalUnit(int cycles) {
+		this.cycles = cycles;
+	}
+
+	public int execute(String type, int op1, int op2) {
+		
+	}
+	
+
+}
