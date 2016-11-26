ae7ce8947eb840ed2ad34b4fd83011bea34acdf0
Tokyo <ahmed.tokyo1@gmail.com>
2016-11-26 07:00:26 +0200

    add block Writing and block Replacement by write policies to memHierarchy

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -14,12 +14,23 @@ public class Utils {
 		String toReturn = Integer.toBinaryString(number);
 		if (number >= 0) {
 			while(toReturn.length() < wordSizeInBits)
-				toReturn = "0" + toReturn;
+				toReturn = '0' + toReturn;
 		}
 		else {
 			toReturn = toReturn.substring(wordSizeInBits);
 		}
 		return toReturn;
+	}
+	
+	public static String generateMask(int length) {
+		String toReturn ="";
+		for(int i = 0; i < length; i++)
+			toReturn += '0';
+		return toReturn;
+	}
+	
+	public static int getWordSizeInBits(){
+		return wordSizeInBits;
 	}
 	
 //	public static void main(String[] args) {
