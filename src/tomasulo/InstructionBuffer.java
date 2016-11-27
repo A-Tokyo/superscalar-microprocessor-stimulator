b23f4285ec293eddf912c76766a99041af57a3c2
Mostafa Ebrahim <mostafa.ebrahim@hotmail.com>
2016-11-28 01:39:29 +0200

    fix ... bugs

---
+++ src/tomasulo/InstructionBuffer.java
@@ -0,0 +1,81 @@
+package tomasulo;
+
+public class InstructionBuffer {
+ 
+	int head;
+	int tail;
+	Instruction [] Buffer ;
+		
+	public InstructionBuffer(int size) {
+		this.Buffer=new Instruction[size];
+	}
+	
+	public  boolean Empty_Instruction_Buffer(){
+		for(int i = 0; i < Buffer.length; i++) {
+			if(Buffer[i] != null)
+				return false;
+		}
+		return true;
+	}
+		
+	
+	 public boolean  Full_Instruction_Buffer(){
+		 Instruction Instruction_Buffer_tail=this.Buffer[tail];
+//		 int length = this.Buffer.length ;
+		
+		 if(Instruction_Buffer_tail==null ){ 
+			 return false;
+		 
+		} else return true;
+	 }
+	
+	public boolean Add_To_Instruction_Buffer (Instruction addIt){
+		
+		if (Buffer[tail] == null) {
+			Buffer[tail] = addIt;
+			tail++;
+			
+			if(tail == Buffer.length)
+				tail = 0; // TO BE like circle  
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}// end method
+	
+	public Instruction remove_element_from_Instruction_Buffer(){
+		int length = this.Buffer.length  ;
+		 Instruction Instruction_Buffer_head=this.Buffer[head];
+		 
+		// for(int head =0 ; head <length ;head++){
+//			 Instruction Instruction_Buffer_head=this.Buffer[head];
+			 
+			 if(Instruction_Buffer_head ==null){ // head points to null 
+				 return null;
+			 }
+			
+			 Instruction_Buffer_head =null;
+			 head++;
+		// }
+		 
+		 if (length==head)
+			 head =0;
+			 return  Instruction_Buffer_head ;
+		
+	}
+	
+	public void Flush(){ // clean all 
+		int length = this.Buffer.length  ;
+		for(int i=0;i<length ; i++){			
+			Buffer[i] = null;
+		}
+		this.head=0;
+		this.tail=0 ;
+	
+	}
+	
+	
+
+}
