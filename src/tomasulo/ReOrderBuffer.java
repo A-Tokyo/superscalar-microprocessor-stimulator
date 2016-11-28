3a83769c297e126d1e6440069ab5f3fb179206c7
Mostafa Ebrahim <mostafa.ebrahim@hotmail.com>
2016-11-28 15:19:30 +0200

    Update ReOrderBuffer.java

--- src/tomasulo/ReOrderBuffer.java
+++ src/tomasulo/ReOrderBuffer.java
@@ -1,56 +1,59 @@
 package tomasulo;
 
-// NOTE: Still in the works, missing some important functionality
-// Committed for visibility / integration purposes
-public class ReOrderBuffer {
 
-	// set as 6 for simplicity; should maybe be taken from user - if so, simply change this value
-	public int sizeOfROB;
+public class ROBEntry {
 	
-	private ROBEntry[] buffer;
-	private ROBEntry head;
-	private int headPosition;
-	private ROBEntry tail;
-	private int tailPosition;
+	private String instructionType;
+	private long instructionDestination;
+	private int instructionValue;
+	private boolean Ready;
 	
-	public ReOrderBuffer(int sizeOfROB) {
-		this.sizeOfROB = sizeOfROB;
-		this.buffer = new ROBEntry[this.sizeOfROB];
-		this.headPosition = 0;
-		this.tailPosition = this.headPosition;
-		this.head = this.buffer[this.headPosition];
-		this.tail = this.buffer[this.tailPosition];
+	public ROBEntry(String instructionType, long instructionDestination, int instructionValue, boolean Ready) {
+		this.instructionType = instructionType;
+		this.instructionDestination = instructionDestination;
+		this.instructionValue = instructionValue;
+		this.Ready = Ready;
+	}
+	
+	public String getInstructionType() {
+		return instructionType;
+	}
+	public void setInstructionType(String instructionType) {
+		this.instructionType = instructionType;
 	}
 
-	public void incrementHead() {
-		
-		if (this.headPosition == this.sizeOfROB - 1) {
-			this.headPosition = 0;
+	
+	public long getInstructionDestination() {
+		return instructionDestination;
+	}
+	public void setInstructionDestination(long instructionDestination) {
+		this.instructionDestination = instructionDestination;
+	}
+	public int getInstructionValue() {
+		return instructionValue;
+	}
+	public void setInstructionValue(int instructionValue) {
+		this.instructionValue = instructionValue;
+	}
+	public boolean isReady() {
+		return Ready;
+	}
+	public void setReady(boolean ready) {
+		Ready = ready;
+	}
+	
+	public boolean isEqual(ROBEntry otherEntry) {
+		if (this.instructionType != otherEntry.instructionType) {
+			return false;
+		} else if (this.instructionDestination != otherEntry.instructionDestination) {
+			return false;
+		} else if (this.instructionValue != otherEntry.instructionValue) {
+			return false;
+		} else if (this.Ready != otherEntry.Ready) {
+			return false;
 		} else {
-			this.headPosition += 1;
+			return true;
 		}
-		
-		this.head = this.buffer[this.headPosition];
-		
 	}
-	
-	public void incrementTail() {
-		
-		if (this.tailPosition == this.sizeOfROB - 1) {
-			this.tailPosition = 0;
-		} else {
-			this.tailPosition += 1;
-		}
-		
-		this.tail = this.buffer[this.tailPosition];
-	}
-	
-	public void enQueue(ROBEntry entry) {
-		this.tail = entry;
-		incrementTail();
-	}
-	
 	
 }
-
-
